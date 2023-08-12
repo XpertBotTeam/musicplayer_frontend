d5b8d4beff87464641df5d7000d8a1cfdd427158
Ali-xD YT <alireda_hajj@hotmail.com>
2023-08-12 12:27:28 +0300

    Addint login, register pages + routers setup

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,49 @@
+<script>
+export default {
+  name: "AppHeader",
+};
+</script>
+
+
+<template>
+  <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+      <router-link class="navbar-brand" to="/">Music App</router-link>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarText"
+        aria-controls="navbarText"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarText">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <router-link class="nav-link" to="/">Home</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/">Another Page</router-link>
+          </li>
+        </ul>
+        <span class="navbar-text">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <router-link class="nav-link active" to="/login"
+                >Login</router-link
+              >
+            </li>
+            <li class="nav-item">
+              <router-link class="nav-link" to="/register"
+                >Register</router-link
+              >
+            </li>
+          </ul>
+        </span>
+      </div>
+    </div>
+  </nav>
+</template>
---
+++ src/components/AppHome.vue
@@ -0,0 +1,10 @@
+<script>
+export default {
+  name: 'AppHome'
+}
+</script>
+
+
+<template>
+    <h1>Hello this is the home page</h1>
+</template>
